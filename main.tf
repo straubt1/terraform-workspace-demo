69673277ef698d1fcd1e062014b38200efcdf6ce
Tom Straub <tstraub@hashicorp.com>
2022-11-14 14:27:18 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    demo = {
+      source = "app.terraform.io/tfc-demo-org/demo"
+      version = "0.0.1"
+    }
+  }
+}
+
 resource "demo_example" "example" {
   configurable_attribute = "some-resource-value"
 }
